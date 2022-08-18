639e6cf48b33dc1e87f246f1d52d491f1b010e1e
Manyeya <siemakhutso@gmail.com>
2022-08-18 12:31:03 +0200

    new change

--- server/src/api/faculty/controllers/faculty.ts
+++
@@ -1,7 +0,0 @@
-/**
- *  faculty controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::faculty.faculty');
--- server/src/api/university/routes/university.ts
+++
@@ -1,7 +0,0 @@
-/**
- * university router.
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::university.university');
